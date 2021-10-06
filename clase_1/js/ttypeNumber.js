6139b71308c9449343b7af9f4388e4853d1acb4c
kucko <felipehg3@gmail.com>
2021-10-05 21:56:02 -0500

    clase 1 javaScript

---
+++ clase_1/js/ttypeNumber.js
@@ -0,0 +1,14 @@
+var miPrimeraVariable = "Hola todos";
+/*
+NaN - Not-A-Number
+*/
+console.log(isNaN(miPrimeraVariable));
+
+var miPrimeraVariable = "Hola todos";
+var miPrimeraVariable = 5;
+console.log(isNaN(miPrimeraVariable));
+//Maximo valor soportado en JS
+console.log(Number.MAX_VALUE);
+//Minimo valor soportado en JS
+console.log(Number.MIN_VALUE);
+console.log
---
+++ clase_1/js/typeBoolean.js
@@ -0,0 +1,18 @@
+//Tipo Booleano
+//true
+var boleanoUno = true;
+var boleanoDos = false;
+
+console.log(boleanoUno);
+console.log(boleanoDos);
+
+//0, -0, null, false, NaN, undefined, ""
+console.log(Boolean(0));
+console.log(Boolean(-0));
+console.log(Boolean(null));
+console.log(Boolean(false));
+console.log(Boolean(NaN));
+console.log(Boolean(undefined));
+console.log(Boolean(""));
+console.log(Boolean(''));
+console.log(Boolean(``));
